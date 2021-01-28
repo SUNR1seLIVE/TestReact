335f8043caddcf631b9c0c6eecfe984765f4c8d1
Alexandr Yaroslav <47974131+SUNR1seLIVE@users.noreply.github.com>
2021-01-28 22:25:36 +0300

    Добавление в репозиторий

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+	return <header className='header'>
+		<img src='https://p7.hiclipart.com/preview/956/991/647/v-for-vendetta-clip-art-anonymous-mask.jpg' />
+	</header>;
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+	return <div className='content'>
+		<div>
+			<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREEt1hdouHY3OO6LqQrwJqIDp0By5iwnbyHQ&usqp=CAU' />
+		</div>
+		<div>
+			Ava + description
+		</div>
+		<div>
+			My posts
+			<div>
+				New post
+			</div>
+			<div>
+				<div>
+					Post 1
+				</div>
+				<div>
+					Post 2
+				</div>
+			</div>
+		</div>
+	</div>;
+}
+
+export default Profile
