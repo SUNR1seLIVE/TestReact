c1d2cff77a972cf7a9817b45a7817bceca298481
Alexandr Yaroslav <47974131+SUNR1seLIVE@users.noreply.github.com>
2021-01-30 02:34:21 +0300

    Вынес данные в index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,44 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
-
-const DialogItem = (props) => {
-	let path = '/dialogs/' + props.id;
-	return (
-		<div className={s.dialog}>
-			<NavLink to={path}>{props.name}</NavLink>
-		</div>
-	)
-}
-
-const Message = (props) => {
-	return (
-		<div className={s.massage}>{props.massage}</div>
-	)
-}
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message'
 
 const Dialogs = (props) => {
 
-	let dialogs = [
-		{ id: 1, name: 'Дима' },
-		{ id: 2, name: 'Андрей' },
-		{ id: 3, name: 'Света' },
-		{ id: 4, name: 'Саша' },
-		{ id: 5, name: 'Виктор' },
-		{ id: 6, name: 'Валя' }
-	];
-	let messages = [
-		{ id: 1, message: 'Привет1' },
-		{ id: 2, message: 'Привет2' },
-		{ id: 3, message: 'Привет3' },
-		{ id: 4, message: 'Привет4' },
-		{ id: 5, message: 'Привет5' },
-		{ id: 6, message: 'Привет6' },
-		{ id: 7, message: 'Привет7' }
-	];
-
-	let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id}/> );
-	let messagesElements = messages.map( m => <Message massage={m.message} /> );
+	let dialogsElements = props.dialogs.map( d => <DialogItem name={d.name} id={d.id}/> );
+	let messagesElements = props.messages.map( m => <Message massage={m.message} /> );
 
 	return (
 		<div className={s.dialogs}>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,31 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let posts = [
+	{id: 1, message: 'Привет, как ты?', likesCount: 12},
+	{id: 2, message: 'Это мой первый пост', likesCount: 11}
+];
+let dialogs = [
+	{ id: 1, name: 'Дима' },
+	{ id: 2, name: 'Андрей' },
+	{ id: 3, name: 'Света' },
+	{ id: 4, name: 'Саша' },
+	{ id: 5, name: 'Виктор' },
+	{ id: 6, name: 'Валя' }
+];
+let messages = [
+	{ id: 1, message: 'Привет1' },
+	{ id: 2, message: 'Привет2' },
+	{ id: 3, message: 'Привет3' },
+	{ id: 4, message: 'Привет4' },
+	{ id: 5, message: 'Привет5' },
+	{ id: 6, message: 'Привет6' },
+	{ id: 7, message: 'Привет7' }
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App posts={posts} dialogs={dialogs} messages={messages} />
   </React.StrictMode>,
   document.getElementById('root')
 );
