ed29be77177af2a79f66968bfed03288c41ae1d4
Alexandr Yaroslav <47974131+SUNR1seLIVE@users.noreply.github.com>
2021-01-30 21:12:00 +0300

    добавил observer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import Music from './components/Music/Music';
 import News from './components/News/News';
 import Setting from './components/Setting/Setting';
@@ -12,19 +12,17 @@ import Setting from './components/Setting/Setting';
 
 const App = (props) => {
   return (
-		<BrowserRouter>
-			<div className='app-wrapper'>
-				<Header />
-				<Navbar />
-				<div className='app-wrapper-content'>
-					<Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage} /> } />
-					<Route path='/profile' render={ () => <Profile profilePage={props.state.profilePage} addPost={props.addPost} updateNewPostText={props.updateNewPostText} /> } />
-					<Route path='/news' render={ () => <News /> } />
-					<Route path='/music' render={ () => <Music /> } />
-					<Route path='/setting' render={ () => <Setting /> } />
-				</div>
+		<div className='app-wrapper'>
+			<Header />
+			<Navbar />
+			<div className='app-wrapper-content'>
+				<Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage} /> } />
+				<Route path='/profile' render={ () => <Profile profilePage={props.state.profilePage} addPost={props.addPost} updateNewPostText={props.updateNewPostText} /> } />
+				<Route path='/news' render={ () => <News /> } />
+				<Route path='/music' render={ () => <Music /> } />
+				<Route path='/setting' render={ () => <Setting /> } />
 			</div>
-		</BrowserRouter>
+		</div>
   )
 }
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,4 @@
-import { rerenderEntireTree } from "./../render";
+let rerenderEntireTree;
 
 let state = {
 	profilePage: {
@@ -29,7 +29,7 @@ let state = {
 	sidebar: {}
 }
 
-export let addPost = () => {
+export const addPost = () => {
 	let newPost = {
 		id: 5,
 		message: state.profilePage.newPostText,
@@ -40,9 +40,15 @@ export let addPost = () => {
 	rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 	state.profilePage.newPostText = newText;	
 	rerenderEntireTree(state);
 }
 
+export const subscribe = (observer) => {
+	rerenderEntireTree = observer;
+}
+
 export default state;
+
+//store - OOP
