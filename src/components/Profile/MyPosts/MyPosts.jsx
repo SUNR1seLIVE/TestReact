bcadd0968e07765a753e52391aa8fe6c90ebfbd7
Alexandr Yaroslav <47974131+SUNR1seLIVE@users.noreply.github.com>
2021-01-31 00:47:30 +0300

    Добавил actionCreator, actionType

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import {updateNewPostTextActionCriator, addPostActionCriator} from './../../../redux/state'
 
 const MyPosts = (props) => {
 
-	let postsElement = props.posts.map( p => <Post message={p.message} likeCount={p.likesCount} /> );
+	let postsElement = props.posts.map(p => <Post message={p.message} likeCount={p.likesCount} />);
 
 	let newPostElement = React.createRef();
 
 	let addPost = () => {
-		props.dispatch({type: 'ADD-POST'});
+		props.dispatch(addPostActionCriator());
 	}
 
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
-		let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text}
+		let action = updateNewPostTextActionCriator(text);
 		props.dispatch(action);
 	}
 
@@ -23,14 +24,14 @@ const MyPosts = (props) => {
 			<h3>My posts</h3>
 			<div>
 				<div>
-					<textarea onChange={ onPostChange } ref={newPostElement} value={props.newPostText}/>
+					<textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
 				</div>
 				<div>
-					<button onClick={ addPost }>Add post</button>
+					<button onClick={addPost}>Add post</button>
 				</div>
 			</div>
 			<div className={s.posts}>
-				{ postsElement }
+				{postsElement}
 			</div>
 		</div>
 	)
