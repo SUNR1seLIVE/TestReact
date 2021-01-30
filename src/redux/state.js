e42007e2aba97d0af77068b077842f59bb0e31cd
Alexandr Yaroslav <47974131+SUNR1seLIVE@users.noreply.github.com>
2021-01-30 18:12:39 +0300

    FLUX Круговорот на каждый символ

--- src/redux/state.js
+++ src/redux/state.js
@@ -5,7 +5,8 @@ let state = {
 		posts: [
 			{id: 1, message: 'Привет, как ты?', likesCount: 12},
 			{id: 2, message: 'Это мой первый пост', likesCount: 11}
-		]
+		],
+		newPostText: ''
 	},
 	dialogsPage: {
 		dialogs: [
@@ -28,14 +29,19 @@ let state = {
 	sidebar: {}
 }
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
 	let newPost = {
 		id: 5,
-		message: postMessage,
+		message: state.profilePage.newPostText,
 		likesCount: 0
 	}
 	state.profilePage.posts.push(newPost);
+	state.profilePage.newPostText = '';
+	rerenderEntireTree(state);
+}
 
+export let updateNewPostText = (newText) => {
+	state.profilePage.newPostText = newText;	
 	rerenderEntireTree(state);
 }
 
